48918978dd92f98fb0cc4faeeb37f6dc6e803dba
hirasoomro <hiraasoomro@gmail.com>
2023-06-08 11:07:19 -0700

    trying to add tailwind

--- Professionalportfolio/src/pages/About.js
+++ Professionalportfolio/src/pages/About.js
@@ -3,8 +3,8 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-import { FaSchool } from 'react-icons/fa';
-import { FaNetworkWired } from 'react-icons/fa';
+import { FaSchool } from "react-icons/fa";
+import { FaNetworkWired } from "react-icons/fa";
 import profilePhoto from "../assets/profilephoto.JPG";
 import "../styles/about.css";
 
@@ -21,31 +21,29 @@ function Experience() {
           iconStyle={{ background: "#3e497a", color: "#fff" }}
         >
           <div className="skills">
-            <h1>Skills</h1>
+            <h1 className="text-3xl font-bold">Skills</h1>
             <ol className="list">
               <li className="item">
-                <h2>Front-End</h2>
+                <h2 className="text-xl">Front-End</h2>
                 <span>
-                  ReactJS, Redux, HTML, CSS, React Native, NPM,
-                  BootStrap, TailwindCSS, StyledComponents
+                  ReactJS, Redux, HTML, CSS, NPM, BootStrap,
+                  TailwindCSS
                 </span>
               </li>
               <li className="item">
-                <h2>Back-End</h2>
+                <h2 className="text-xl">Back-End</h2>
                 <span>
                   NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
-                  MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
+                  MySQL, MongoDB
                 </span>
               </li>
               <li className="item">
-                <h2>Languages</h2>
+                <h2 className="text-xl">Languages</h2>
                 <span>JavaScript, MySQL</span>
               </li>
             </ol>
           </div>
-          <h3 className="vertical-timeline-element-title">
-            Hira S.
-          </h3>
+          <h3 className="vertical-timeline-element-title">Hira S.</h3>
           <p>
             Hi! Welcome to my page. I am a full stack web developer who recently graduated from a full stack web development bootcamp at the University of California Irvine.
             I have worked in customer service for over 3 years before becoming an application specialist. After that, I worked in a closed-door pharmacy and urgent care,
@@ -54,7 +52,9 @@ function Experience() {
           </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className={`vertical-timeline-element--education ${selectedSection === 'about' ? 'selected' : ''}`}
+          className={`vertical-timeline-element--education ${
+            selectedSection === "about" ? "selected" : ""
+          }`}
           date="2010 - 2014"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<FaSchool />}
@@ -66,7 +66,9 @@ function Experience() {
           <p>High School Diploma</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className={`vertical-timeline-element--education ${selectedSection === 'skills' ? 'selected' : ''}`}
+          className={`vertical-timeline-element--education ${
+            selectedSection === "skills" ? "selected" : ""
+          }`}
           date="2010 - 2014"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<FaSchool />}
@@ -80,21 +82,23 @@ function Experience() {
           <p>Psychology</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className={`vertical-timeline-element--work ${selectedSection === 'projects' ? 'selected' : ''}`}
-          date="2016 - 2019"
+          className={`vertical-timeline-element--work ${
+            selectedSection === "projects" ? "selected" : ""
+          }`}
+          date="2019 - 2020"
           iconStyle={{ background: "#e9d35b", color: "#fff" }}
           icon={<FaNetworkWired />}
         >
           <h3 className="vertical-timeline-element-title">
             Applicant Support Specialist
           </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            El Segundo, CA
-          </h4>
+          <h4 className="vertical-timeline-element-subtitle">El Segundo, CA</h4>
           <p>Government contractors looking for work.</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className={`vertical-timeline-element--work ${selectedSection === 'projects' ? 'selected' : ''}`}
+          className={`vertical-timeline-element--work ${
+            selectedSection === "projects" ? "selected" : ""
+          }`}
           date="2020 - 2021"
           iconStyle={{ background: "#e9d35b", color: "#fff" }}
           icon={<FaNetworkWired />}
@@ -102,9 +106,7 @@ function Experience() {
           <h3 className="vertical-timeline-element-title">
             Honeybee Health pharmacy
           </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Los Angeles, CA
-          </h4>
+          <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
           <p>Closed Door Pharmacy; a new way to buy medicine.</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
